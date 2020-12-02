0bae447de7b9af94a6d337c21578474c949c5cfa
Zint3r <48679829+Zint3r@users.noreply.github.com>
2020-12-02 20:08:16 +0300

    Final updates and build

---
+++ Survive/Assets/Scripts/ProgressScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressScript : MonoBehaviour
+{
+    [SerializeField] private GameObject panelObj = null;
+    [SerializeField] private Image imgObject = null;
+    [SerializeField] private Sprite foodSprite = null;
+    [SerializeField] private Sprite broomSprite = null;
+    [SerializeField] private Sprite ballSprite = null;
+    [SerializeField] private Sprite syringeSprite = null;    
+    public void FoodActive()
+    {
+        panelObj.SetActive(true);
+        StartCoroutine(FillingImg(foodSprite));
+    }
+    public void BroomActive()
+    {
+        panelObj.SetActive(true);
+        StartCoroutine(FillingImg(broomSprite));
+    }
+    public void BallActive()
+    {
+        panelObj.SetActive(true);
+        StartCoroutine(FillingImg(ballSprite));
+    }
+    public void SyringeActive()
+    {
+        panelObj.SetActive(true);
+        StartCoroutine(FillingImg(syringeSprite));
+    }    
+    private IEnumerator FillingImg(Sprite spr)
+    {
+        while (true)
+        {
+            imgObject.sprite = spr;
+            imgObject.fillAmount -= 0.05f;
+            if (imgObject.fillAmount == 0)
+            {
+                panelObj.SetActive(false);
+                imgObject.fillAmount = 1;
+                break;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
